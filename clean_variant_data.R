5f43a0bc8268af3cc7b3145c9410f59ccc61953e
Zachary Susswein <zsusswein@gmail.com>
2022-09-23 09:36:30 -0400

    Tweak transmissibility multipliers to match Yang et al.

--- clean_variant_data.R
+++ clean_variant_data.R
@@ -7,12 +7,16 @@ variant_names <- tibble(name = c('alpha', 'beta', 'gamma', 'delta',  'epsilon', 
                         lineage = c('B.1.1.7', 'B.1.351', 'P.1', 'B.1.617.2', 'B.1.427', 'B.1.429', 'B.1.525', 'B.1.617.1', 'B.1.526', 'P.2', 'other'))
 
 transmissibility_over_wildtype <- tibble(lineage = c('alpha', 'beta', 'gamma', 'delta', 'epsilon', 'eta', 'kappa', 'iota', 'zeta', 'other', 'omicron', 'mu'),
-                                         transmissibility_multiplier = c(1.75, 1.55, 1.25, 1.97, 1.24, 1.29, 1.48, 1.35, 1, 1, 4.65, 2.25))
+                                         transmissibility_multiplier = c(1.5, 1.6, 1.4, 2.4, 1.24, 1.29, 1.48, 1.35, 1, 1, 4.65, 2.25))
 
 # for Omicron: https://www.medrxiv.org/content/10.1101/2021.12.19.21268038v1.full.pdf
 # In SA, tramsissibility is ~3x above background which is almost all Delta
 # review of transmissibilities here: https://www.cambridge.org/core/services/aop-cambridge-core/content/view/39243FCC3CED73D5F1D94E497F8823D3/S0950268821002430a.pdf
 # taking mu from here and assuming minimal immune escape https://www.medrxiv.org/content/10.1101/2021.12.09.21267544v1.full.pdf
+# https://www.nature.com/articles/s41467-021-25913-9
+# https://www.sciencedirect.com/science/article/pii/S2666379122000908
+# Alpha, beta, gamma: https://www.nature.com/articles/s41467-021-25913-9
+
 
 df.fips <- read_csv('data/state_and_county_fips_master.csv') %>% 
   mutate(fips = if_else(fips ==02270, 02158, fips),
